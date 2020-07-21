3f9ecaec66ff397d4950fd7dee628a988dc5eb35
ucking44 <ucking4niga@yahoo.com>
2020-07-21 09:47:24 +0100

    initial release

--- app/Http/Controllers/BannerController.php
+++ app/Http/Controllers/BannerController.php
@@ -19,6 +19,11 @@ class BannerController extends Controller
      */
     public function index()
     {
+        // $about = About::all();
+        // $news = News::all();
+        // $banner = Banner::all();
+        //$media = Media::all();
+        //$background = Background::all();
         $banner = Banner::all();
         return view('background', compact('banner', 'background', 'media'));
     }
